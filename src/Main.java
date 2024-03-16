a22bad1ed338301cf3bff2a4009d1bf48ea3eaef
emiradylbekov <emir.adylbekovich@gmail.com>
2024-03-16 18:53:44 +0600

    first commit

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+public class Main {
+    public static void main(String[] args) {
+        BankAccount account = new BankAccount(0.0);
+        account.deposit(15000);
+        while (true) {
+            try {
+                account.withDraw(6000);
+                System.out.println("Снято 6000 сом, текущий баланс: " + account.getAmount() + " сом.");
+            } catch (LimitException e) {
+                try {
+                    account.withDraw(account.getAmount());
+                } catch (LimitException ex) {
+                    throw new RuntimeException(ex);
+                }
+                System.out.println("Исключение: " + e.getMessage());
+                System.out.println("Максимальная доступная сумма для снятия: " + e.getRemainingAmount() + " сом.");
+                System.out.println("Снято: " + e.getRemainingAmount() + "сом");
+                System.out.println("Остаток на счете : 0 сом");
+                break;
+            }
+        }
+
+    }
+
+}
